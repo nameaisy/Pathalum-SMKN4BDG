97448e77a1012a7e61a52f107e07bd755cf94cc2
AzisBasyari <azisbasyari3010@gmail.com>
2022-01-08 03:15:49 +0700

    Update Database

--- pathalum.sql
+++ pathalum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2022 at 01:18 PM
+-- Generation Time: Jan 07, 2022 at 09:12 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -39,15 +39,81 @@ CREATE TABLE `alumni` (
   `tahun_masuk` varchar(10) DEFAULT NULL,
   `tahun_lulus` varchar(10) DEFAULT NULL,
   `no_ijazah` varchar(50) DEFAULT NULL,
-  `no_skhun` varchar(50) DEFAULT NULL
+  `no_skhun` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `alumni`
 --
 
-INSERT INTO `alumni` (`id`, `id_users`, `jenis_kelamin`, `alamat`, `tempat_lahir`, `tanggal_lahir`, `no_telepon`, `jurusan`, `tahun_masuk`, `tahun_lulus`, `no_ijazah`, `no_skhun`) VALUES
-(1, 2, 'Laki-laki', 'PH Pride', 'Bandung', '2000-10-30', '08123456789', 'Rekayasa Perangkat Lunak', '2019', '2022', 'xxxxxxxxxxx', 'xxxxxxxxxxx');
+INSERT INTO `alumni` (`id`, `id_users`, `jenis_kelamin`, `alamat`, `tempat_lahir`, `tanggal_lahir`, `no_telepon`, `jurusan`, `tahun_masuk`, `tahun_lulus`, `no_ijazah`, `no_skhun`, `status`) VALUES
+(1, 2, 'Laki-laki', 'PH Pride', 'Bandung', '2000-10-30', '08123456789', 'Rekayasa Perangkat Lunak', '2019', '2022', 'xxxxxxxxxxx', 'xxxxxxxxxxx', 'Tidak Bekerja Ataupun Kuliah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deskripsi_kuliah`
+--
+
+CREATE TABLE `deskripsi_kuliah` (
+  `id` int(15) NOT NULL,
+  `id_users` int(15) NOT NULL,
+  `nama_perguruan_tinggi` varchar(100) DEFAULT NULL,
+  `alamat_perguruan_tinggi` varchar(100) DEFAULT NULL,
+  `fakultas` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(100) DEFAULT NULL,
+  `semester` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deskripsi_kuliah`
+--
+
+INSERT INTO `deskripsi_kuliah` (`id`, `id_users`, `nama_perguruan_tinggi`, `alamat_perguruan_tinggi`, `fakultas`, `jurusan`, `semester`) VALUES
+(1, 2, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deskripsi_pekerjaan`
+--
+
+CREATE TABLE `deskripsi_pekerjaan` (
+  `id` int(15) NOT NULL,
+  `id_users` int(15) NOT NULL,
+  `nama_perusahaan` varchar(100) DEFAULT NULL,
+  `alamat_perusahaan` varchar(100) DEFAULT NULL,
+  `posisi` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deskripsi_pekerjaan`
+--
+
+INSERT INTO `deskripsi_pekerjaan` (`id`, `id_users`, `nama_perusahaan`, `alamat_perusahaan`, `posisi`) VALUES
+(1, 2, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `deskripsi_wirausaha`
+--
+
+CREATE TABLE `deskripsi_wirausaha` (
+  `id` int(15) NOT NULL,
+  `id_users` int(15) NOT NULL,
+  `nama_usaha` varchar(100) DEFAULT NULL,
+  `alamat_usaha` varchar(100) DEFAULT NULL,
+  `jenis_produk` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deskripsi_wirausaha`
+--
+
+INSERT INTO `deskripsi_wirausaha` (`id`, `id_users`, `nama_usaha`, `alamat_usaha`, `jenis_produk`) VALUES
+(1, 2, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -84,6 +150,27 @@ ALTER TABLE `alumni`
   ADD KEY `id_users` (`id_users`);
 
 --
+-- Indexes for table `deskripsi_kuliah`
+--
+ALTER TABLE `deskripsi_kuliah`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`);
+
+--
+-- Indexes for table `deskripsi_pekerjaan`
+--
+ALTER TABLE `deskripsi_pekerjaan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`);
+
+--
+-- Indexes for table `deskripsi_wirausaha`
+--
+ALTER TABLE `deskripsi_wirausaha`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -97,6 +184,24 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `alumni`
 --
 ALTER TABLE `alumni`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `deskripsi_kuliah`
+--
+ALTER TABLE `deskripsi_kuliah`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `deskripsi_pekerjaan`
+--
+ALTER TABLE `deskripsi_pekerjaan`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `deskripsi_wirausaha`
+--
+ALTER TABLE `deskripsi_wirausaha`
   MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -114,6 +219,24 @@ ALTER TABLE `users`
 --
 ALTER TABLE `alumni`
   ADD CONSTRAINT `alumni_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `deskripsi_kuliah`
+--
+ALTER TABLE `deskripsi_kuliah`
+  ADD CONSTRAINT `deskripsi_kuliah_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `deskripsi_pekerjaan`
+--
+ALTER TABLE `deskripsi_pekerjaan`
+  ADD CONSTRAINT `deskripsi_pekerjaan_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `deskripsi_wirausaha`
+--
+ALTER TABLE `deskripsi_wirausaha`
+  ADD CONSTRAINT `deskripsi_wirausaha_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
