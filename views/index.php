be1962e5361f6337f0e1b2a9cd738960be356e3d
fabyanrafisi <fabyanrafisi@gmail.com>
2022-01-12 18:38:34 +0700

    update bagian halaman utama

--- views/index.php
+++ views/index.php
@@ -8,6 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/home.css">
+
 
     <title>Pathalum</title>
 </head>
@@ -18,9 +20,23 @@
               <img src="../assets/logo/SMKN4.svg" alt="" width="60" height="60" class="smkn4">
               SMK NEGERI 4 BANDUNG
             </li>
-            
-    <div class="main-container">
-        
+        <div class="center">
+            <button class="btn btn-primary btn-masuk" type="submit" name="masuk" value="masuk">Login</button>
+            <button class="btn btn-primary btn-register" type="submit" name="masuk" value="masuk">Register</button> 
+        </div>
     </div>
+            
+    <div class="main-container box-form">
+        <h3>PATHALUM</h3>
+        <h5 style="text-align: center;">Pathalum adalah sebuah proyek yang memiliki tujuan untuk mempermudah hubin dan alumni sebagai jejaring sosial yang dapat memberikan informasi kepada para alumni. Dengan adanya proyek ini diharapkan dapat memberikan kemudahan bagi alumni dan hubin dalam memperoleh informasi yang berhubungan dengan para alumni. PATHALUM diharapkan bisa bekerja dengan baik agar kebingungan dalam mencari kebutuhan informasi mengenai para alumni.</h5>
+    </div>
+
+    <section id="footer">
+        <ul>
+        <img src="../assets/logo/CASADEV.svg" alt="" width="60" height="60" class="casadev">
+        </ul>
+
+        <p id="me">Created by CASADEV</a>.</p></a>
+    </section>
 </body>
 </html>
