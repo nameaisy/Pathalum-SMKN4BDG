103ee5669ed3706a77e9248c06fc30ad63029335
AzisBasyari <azisbasyari3010@gmail.com>
2022-02-17 10:52:53 +0700

    Update index

--- views/index.php
+++ views/index.php
@@ -8,6 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/home.css">
+    <link rel="icon" href="../assets/logo/SMKN4.svg">
+
 
     <title>Pathalum</title>
 </head>
@@ -18,9 +21,60 @@
               <img src="../assets/logo/SMKN4.svg" alt="" width="60" height="60" class="smkn4">
               SMK NEGERI 4 BANDUNG
             </li>
-            
-    <div class="main-container">
-        
+        <div class="center">
+           <a href="../views/login.php"><button class="btn btn-primary btn-masuk" type="submit" name="masuk" value="masuk">Login</button></a>
+           <a href="../views/register.php"><button class="btn btn-primary btn-register" type="submit" name="register" value="register">Register</button></a> 
+        </div>
     </div>
+            
+    <div class="main-container box-form">
+        <h3>PATHALUM</h3>
+        <h5 style="text-align: justify  ;">Pathalum adalah sebuah proyek yang memiliki tujuan untuk mempermudah hubin dan alumni sebagai jejaring sosial yang dapat memberikan informasi kepada para alumni. Dengan adanya proyek ini diharapkan dapat memberikan kemudahan bagi alumni dan hubin dalam memperoleh informasi yang berhubungan dengan para alumni. PATHALUM diharapkan bisa bekerja dengan baik agar kebingungan dalam mencari kebutuhan informasi mengenai para alumni.</h5>
+    </div>
+
+    <div class="">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7921.113822071869!2d107.6220125!3d-6.9434366!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e86413eb50ad%3A0xf9930b5268e3fee1!2sSMKN%204%20Bandung!5e0!3m2!1sid!2sid!4v1635175235787!5m2!1sid!2sid" width="90%" height="600" style="border-radius: 20px; padding: 0; margin: 0; margin-left:90px; margin-top: 600px; margin-bottom: -5px;" loading="lazy"></iframe>
+    </div>
+    
+    <!-- <footer class="text-white" style="background-color: #2199e9;">
+          <div class="p-4">
+            <section class="sekolah">
+              <div class="row d-flex justify-content-center">
+                  <div class="container" style="width: 75%; padding: 45px 0;">
+                    <div class="row">
+                      <div class="col-auto"><img src="https://smkn4bdg.sch.id/image/smkn4.png" alt="" width="100" height="100" class="limg"></div>
+                      <div class="col-auto"><img src="https://cdn.discordapp.com/attachments/739854236568780831/943361896856317992/LOGO_CASADEV_FORMAL_BLACK_page-0001-removebg-preview.png" alt="" width="150" height="150" class="limg"></div>
+             
+                      <div class="row">
+                        <div class="col" style="padding-left: 300px;">
+                          <h4>SMK Negeri 4 Bandung</h4>
+                          <h6> Kliningan Nomor 6 Buah Batu <br>
+                            Telp/Fax : (022) - 7303736  <br>
+                            Kode Pos : 40264 Kota Bandung <br>
+                            Provinsi Jawa Barat <br>
+                            Indonesia <br>
+                          </h6>
+                        </div>
+                      </div>
+
+                      <div class="col-auto"><div class="line"></div></div>
+                    
+                      <div class="col" style="padding-left: 600px;">
+                          <h4>Check and Borrow</h4>
+                          <h6> Team Project (PATHALUM)
+                            SMKN 4 Bandung  - Rekayasa Perangkat Lunak
+                            Bandung<br>
+                          </h6>
+                        </div> -->
+
+    <section id="footer">
+        <ul>
+          <li>
+            <img src="../assets/logo/CASADEV.svg" alt="" width="60" height="60" class="casadev">
+          </li>
+        </ul>
+
+        <p id="me">Created by CASADEV.</p>
+      </section>
 </body>
 </html>
